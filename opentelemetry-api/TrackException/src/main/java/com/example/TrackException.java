5c128ab2af759510ca87727bdb55f8fc88a6953b
Helen Yang <heya@microsoft.com>
2023-06-08 11:00:17 -0700

    Address comments

---
+++ opentelemetry-api/TrackException/src/main/java/com/example/TrackException.java
@@ -0,0 +1,73 @@
+package com.example;
+
+import com.azure.monitor.opentelemetry.exporter.AzureMonitorExporterBuilder;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.StatusCode;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.sdk.trace.export.SpanExporter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TrackException {
+
+  private static final String CONNECTION_STRING = "<Your Connection String>";
+  private static final Logger log4jLogger = LogManager.getLogger("log4j-logger");
+  protected static final Tracer tracer = initTracer();
+
+  public static void main(String[] args) throws InterruptedException {
+    track();
+    Thread.sleep(6000);
+
+    trackWithLog4j2();
+    Thread.sleep(6000);
+  }
+
+  private static void track() {
+    Span span = tracer.spanBuilder("TrackException").startSpan();
+    // put the span into the current Context
+    try (Scope scope = span.makeCurrent()) {
+      throw new RuntimeException("intentionally throws an exception");
+    } catch (Throwable throwable) {
+      span.setStatus(StatusCode.ERROR, "Something bad happened!");
+      span.recordException(throwable);
+    } finally {
+      span.end(); // Cannot set a span after this call
+    }
+  }
+
+  static void trackWithLog4j2() {
+    Span span = GlobalOpenTelemetry.getTracer("TrackException").spanBuilder("TrackException").startSpan();
+    // put the span into the current Context
+    try (Scope scope = span.makeCurrent()) {
+      log4jLogger.error("This is an exception from log4j2");
+    } catch (Throwable throwable) {
+      span.setStatus(StatusCode.ERROR, "Something bad happened!");
+      span.recordException(throwable);
+    } finally {
+      span.end(); // Cannot set a span after this call
+    }
+  }
+
+  private static Tracer initTracer() {
+    // Create Azure Monitor exporter and configure OpenTelemetry tracer to use this exporter
+    // This should be done just once when application starts up
+    SpanExporter exporter = new AzureMonitorExporterBuilder()
+        .connectionString(CONNECTION_STRING)
+        .buildTraceExporter();
+
+    SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
+        .addSpanProcessor(BatchSpanProcessor.builder(exporter).build())
+        .build();
+
+    OpenTelemetrySdk sdk = OpenTelemetrySdk.builder()
+        .setTracerProvider(tracerProvider)
+        .buildAndRegisterGlobal();
+
+    return sdk.getTracer("TrackException");
+  }
+}
---
+++ opentelemetry-api/TrackTrace/src/main/java/com/example/TrackTrace.java
@@ -0,0 +1,106 @@
+package com.example;
+
+import com.azure.monitor.opentelemetry.exporter.AzureMonitorExporterBuilder;
+import com.azure.monitor.opentelemetry.exporter.implementation.ResourceAttributes;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.logs.GlobalLoggerProvider;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.logs.SdkLoggerProvider;
+import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
+import io.opentelemetry.sdk.logs.export.LogRecordExporter;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.samplers.Sampler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.ThreadContext;
+import org.apache.logging.log4j.message.MapMessage;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TrackTrace {
+
+  private static final String CONNECTION_STRING = "<Your Connection String>";
+  private static final org.apache.logging.log4j.Logger log4jLogger = LogManager.getLogger("log4j-logger");
+  private static final org.slf4j.Logger slf4jLogger = LoggerFactory.getLogger("slf4j-logger");
+
+  public static void main(String[] args) throws InterruptedException {
+    initOpenTelemetry();
+
+    // track a trace
+    trackWithLogback();
+    Thread.sleep(2000); // wait 2 sec
+
+    // track a trace using log4j2
+    trackWithLog4j2();
+    Thread.sleep(2000); // wait 2 sec
+  }
+
+  /**
+   * track with Logback using sl4j
+   */
+  private static void trackWithLogback() {
+    // Log using slf4j API w/ logback backend
+    runWithASpan(
+        () ->
+            slf4jLogger
+                .atInfo()
+                .setMessage("trackWithLogback - a slf4j log message with custom attributes")
+                .addKeyValue("key", "trackWithLogback")
+                .log());
+    runWithASpan(() -> slf4jLogger.info("trackWithLogback - a slf4j log message 2 without custom attributes"));
+  }
+
+  /**
+   * track with Log4j2
+   */
+  private static void trackWithLog4j2() {
+    // Log using log4j2 API
+    Map<String, Object> mapMessage = new HashMap<>();
+    mapMessage.put("key", "trackWithLog4j2");
+    mapMessage.put("message", "trackWithLog4j2 - it's a log4j2 message with custom attributes");
+    runWithASpan(() -> log4jLogger.info(new MapMessage<>(mapMessage)));
+    ThreadContext.clearAll();
+    runWithASpan(() -> log4jLogger.info("trackWithLog4j2 - a log4j log message without custom attributes"));
+  }
+
+  /**
+   * initialize OpenTelemetry using Azure Monitor OpenTelemetry Exporter
+   */
+  private static void initOpenTelemetry() {
+    LogRecordExporter logRecordExporter = new AzureMonitorExporterBuilder()
+        .connectionString(CONNECTION_STRING)
+        .buildLogRecordExporter();
+    OpenTelemetrySdk sdk =
+        OpenTelemetrySdk.builder()
+            .setTracerProvider(SdkTracerProvider.builder().setSampler(Sampler.alwaysOn()).build())
+            .setLoggerProvider(
+                SdkLoggerProvider.builder()
+                    .setResource(
+                        Resource.getDefault().toBuilder()
+                            .put(ResourceAttributes.SERVICE_NAME, "track-trace-example")
+                            .build())
+                    .addLogRecordProcessor(
+                        BatchLogRecordProcessor.builder(logRecordExporter).build())
+                    .build())
+            .build();
+    GlobalOpenTelemetry.set(sdk);
+    GlobalLoggerProvider.set(sdk.getSdkLoggerProvider());
+
+    // Add hook to close SDK, which flushes logs
+    // TODO (heya) use opentelemetry-java autoconfigure when it becomes stable
+    Runtime.getRuntime().addShutdownHook(new Thread(sdk::close));
+  }
+
+  static void runWithASpan(Runnable runnable) {
+    Span span = GlobalOpenTelemetry.getTracer("my-tracer").spanBuilder("my-span").startSpan();
+    try (Scope ignore = span.makeCurrent()) {
+      runnable.run();
+    } finally {
+      span.end();
+    }
+  }
+}
