cd0d5d07e90722098f02b707b9c4b653a763dcfb
Helen Yang <heya@microsoft.com>
2023-06-16 10:44:28 -0700

    Add a new module for slf4j1

--- opentelemetry-api/TrackTrace/SLF4J1/src/main/java/com/example/TrackLog4j2.java
+++ opentelemetry-api/TrackTrace/SLF4J1/src/main/java/com/example/TrackLog4j2.java
@@ -13,60 +13,31 @@ import io.opentelemetry.sdk.logs.export.LogRecordExporter;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.samplers.Sampler;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.ThreadContext;
-import org.apache.logging.log4j.message.MapMessage;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class TrackLog4j2 {
 
   private static final String CONNECTION_STRING = "<Your Connection String>";
-  private static final org.apache.logging.log4j.Logger log4jLogger = LogManager.getLogger("log4j-logger");
-  private static final org.slf4j.Logger slf4j_2_Logger = LoggerFactory.getLogger(TrackLog4j2.class);
+  private static final org.slf4j.Logger slf4j_1_Logger = LoggerFactory.getLogger(TrackLog4j2.class);
 
   public static void main(String[] args) throws InterruptedException {
     initOpenTelemetry();
 
-    // track a trace using log4j2
-    track();
-    Thread.sleep(6000); // wait at least 5 seconds to give batch LogRecord processor time to export
-
-    // track a trace using log4j2 and slf4j-2 logger
-    trackWithSlf4j_2();
+    // track a trace using log4j2 and slf4j_1 logger
+    trackWithSlf4j_1();
     Thread.sleep(8000); // wait at least 5 seconds to give batch LogRecord processor time to export
   }
 
   /**
-   * track with Log4j2
+   * track with log4j2 using slf4j logger
    */
-  private static void track() {
-    // Log using log4j2 API
-    Map<String, Object> mapMessage = new HashMap<>();
-    mapMessage.put("key", "track");
-    mapMessage.put("message", "track - it's a log4j2 message with custom attributes");
-    runWithASpan(() -> log4jLogger.warn(new MapMessage<>(mapMessage)), true);
-    ThreadContext.clearAll();
-    runWithASpan(() -> log4jLogger.error("track - a log4j2 log message without custom attributes"), false);
-  }
-
-  /**
-   * track with log4j2 using slf4j-2 logger
-   */
-  private static void trackWithSlf4j_2() {
-    // Log using slf4j logger with log4j2
+  private static void trackWithSlf4j_1() {
+    // Log using slf4j_1 logger with log4j2
     runWithASpan(
-        () ->
-            slf4j_2_Logger
-                .atWarn()
-                .setMessage("trackWithSlf4j_2 - a slf4j log message with custom attributes")
-                .addKeyValue("key", "trackWithSlf4j_2")
-                .log(), true);
+        () -> slf4j_1_Logger.warn("trackWithSlf4j_1 - a slf4j_1 log message with MDC"), true);
 
-    runWithASpan(() -> slf4j_2_Logger.error("trackWithSlf4j_2 - a slf4j_2 log message without custom attributes"), false);
+    runWithASpan(() -> slf4j_1_Logger.error("trackWithSlf4j_1 - a slf4j_1 log message without MDC"), false);
   }
 
   /**
