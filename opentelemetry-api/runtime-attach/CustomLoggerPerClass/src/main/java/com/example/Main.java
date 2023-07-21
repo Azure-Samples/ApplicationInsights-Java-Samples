6d2ac8f886f3d990edf1a78016f392e2640dde17
Helen <56097766+heyams@users.noreply.github.com>
2023-07-21 10:08:50 -0700

    Add runtime attach sample to customize logging level per class (#98)

---
+++ opentelemetry-api/runtime-attach/CustomLoggerPerClass/src/main/java/com/example/Main.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+  public static void main(String[] args) throws InterruptedException {
+    ApplicationInsights.attach();
+
+    MyLogger1 myLogger1 = new MyLogger1();
+    myLogger1.trackWarn();
+    Thread.sleep(6000); // wait at least 5 seconds to give batch LogRecord processor time to export
+
+    MyLogger2 myLogger2 = new MyLogger2();
+    myLogger2.trackError();
+    Thread.sleep(6000); // wait at least 5 seconds to give batch LogRecord processor time to export
+  }
+}
