5c530c32c0458f39e1f3ac3df07d9842f423a525
heyams <heya@microsoft.com>
2024-03-20 12:03:32 -0700

    Revert

--- opentelemetry-api/java-agent/extensions/CustomSpanExporter/src/main/java/com/example/DemoSpanExporter.java
+++ opentelemetry-api/java-agent/extensions/CustomSpanExporter/src/main/java/com/example/DemoSpanExporter.java
@@ -5,13 +5,10 @@ import io.opentelemetry.sdk.common.CompletableResultCode;
 import io.opentelemetry.sdk.trace.data.EventData;
 import io.opentelemetry.sdk.trace.data.SpanData;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class DemoSpanExporter implements SpanExporter {
 
@@ -33,7 +30,6 @@ public class DemoSpanExporter implements SpanExporter {
             if (events != null && !events.isEmpty()) {
                 for (EventData eventData : events) {
                     if (eventData.getName().trim().contains("Status code 404")) {
-                        System.out.println("#### Found event with name: " + eventData.getName());
                         Attributes attributes = Attributes.builder()
                                 .putAll(spanData.getAttributes())
                                 .put(MY_CUSTOM_ATTRIBUTE_KEY_2, MY_CUSTOM_ATTRIBUTE_VALUE_2)
@@ -41,7 +37,6 @@ public class DemoSpanExporter implements SpanExporter {
                         SpanData customSpanData = new DemoSpanData(spanData, attributes);
                         copiedList.add(customSpanData);
                     } else {
-                        System.out.println("#### Found event with name: " + eventData.getName());
                         copiedList.add(spanData);
                     }
                 }
