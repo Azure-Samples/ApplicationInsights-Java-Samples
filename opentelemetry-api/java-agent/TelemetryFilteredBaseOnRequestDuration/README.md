eb04dbd1f2212abd5360f4b3fd5791ce39f82ddd
heyams <heya@microsoft.com>
2024-03-20 12:00:32 -0700

    Filter out requests with longer than 5 seconds duration

---
+++ opentelemetry-api/java-agent/TelemetryFilteredBaseOnRequestDuration/README.md
@@ -0,0 +1,37 @@
+# Use OpenTelemetry Java Agent Extensions to filter telemetry based on request duration longer than 5 seconds
+
+This is a sample app demonstrating how to filter request telemetry based on request duration.
+
+## Build the extension artifact
+
+To build the extension artifact, from this directory:
+
+`cd ../extensions/FilterSpanBasedOnDuration`
+`../../../../mvnw package`
+
+To run the application with the extension and Java agent, from this directory:
+
+`cd ../../TelemetryFilteredBaseOnRequestDuration`
+
+```
+../../../mvnw package
+export APPLICATIONINSIGHTS_CONNECTION_STRING=<Copy connection string from Application Insights Resource Overview>"
+export APPLICATIONINSIGHTS_SELF_DIAGNOSTICS_LEVEL=debug
+
+ java -javaagent:target/agent/applicationinsights-agent.jar -Dotel.javaagent.extensions=../extensions/FilterSpanBasedOnDuration/target/FilterSpanBasedOnDuration-1.0-SNAPSHOT.jar -jar target/app.jar
+```
+
+Send a request with longer than 5 seconds duration to the application: `curl http://localhost:8080/demo/longDuration`
+Send a request with shorter than 5 seconds duration to the application: `curl http://localhost:8080/demo/shortDuration`
+
+Verify there is a log entry that starts with something as follows:
+  `{"ver":1,"name":"Request","time":`
+
+After it's finished running, go to Application Insights portal logs blade, query the following:
+
+  ```kusto
+  requests
+  | where name == 'myShortDurationSpan'
+  ```
+
+Verify that request with longer than 5 seconds duration is not emitted, i.e. requests table should not have any entry with name `myShortDurationSpan` and none with name "myLongDurationSpan".
--- opentelemetry-api/java-agent/extensions/CustomSpanExporter/src/main/java/com/example/DemoSpanExporter.java
+++ opentelemetry-api/java-agent/extensions/CustomSpanExporter/src/main/java/com/example/DemoSpanExporter.java
@@ -5,10 +5,13 @@ import io.opentelemetry.sdk.common.CompletableResultCode;
 import io.opentelemetry.sdk.trace.data.EventData;
 import io.opentelemetry.sdk.trace.data.SpanData;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class DemoSpanExporter implements SpanExporter {
 
@@ -16,6 +19,7 @@ public class DemoSpanExporter implements SpanExporter {
     private static final String MY_CUSTOM_ATTRIBUTE_VALUE_2 = "myCustomAttributeValue2";
 
     public final SpanExporter delegate;
+
 
     public DemoSpanExporter(SpanExporter delegate) {
         this.delegate = delegate;
@@ -29,6 +33,7 @@ public class DemoSpanExporter implements SpanExporter {
             if (events != null && !events.isEmpty()) {
                 for (EventData eventData : events) {
                     if (eventData.getName().trim().contains("Status code 404")) {
+                        System.out.println("#### Found event with name: " + eventData.getName());
                         Attributes attributes = Attributes.builder()
                                 .putAll(spanData.getAttributes())
                                 .put(MY_CUSTOM_ATTRIBUTE_KEY_2, MY_CUSTOM_ATTRIBUTE_VALUE_2)
@@ -36,6 +41,7 @@ public class DemoSpanExporter implements SpanExporter {
                         SpanData customSpanData = new DemoSpanData(spanData, attributes);
                         copiedList.add(customSpanData);
                     } else {
+                        System.out.println("#### Found event with name: " + eventData.getName());
                         copiedList.add(spanData);
                     }
                 }
