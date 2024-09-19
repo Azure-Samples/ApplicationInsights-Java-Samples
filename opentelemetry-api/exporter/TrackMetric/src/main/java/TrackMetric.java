71a405f9b4b3323e3593806a1a219ccfbe4f256a
heyams <heya@microsoft.com>
2024-09-18 17:07:03 -0700

    Fix

--- opentelemetry-api/exporter/TrackMetric/src/main/java/TrackMetric.java
+++ opentelemetry-api/exporter/TrackMetric/src/main/java/TrackMetric.java
@@ -1,21 +1,16 @@
 import com.azure.monitor.opentelemetry.exporter.AzureMonitorExporterBuilder;
 import com.azure.monitor.opentelemetry.exporter.implementation.SemanticAttributes;
-import io.netty.util.AttributeKey;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.metrics.DoubleHistogram;
 import io.opentelemetry.api.metrics.Meter;
-import io.opentelemetry.api.incubator.metrics.ExtendedDoubleHistogramBuilder;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdkBuilder;
 
-import java.util.List;
 import java.util.UUID;
 
-import static java.util.Arrays.asList;
-
 public class TrackMetric {
-    private static final String CONNECTION_STRING = "InstrumentationKey=5504d13f-a841-4b32-9003-8e04cc67d2b9;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/;ApplicationId=747dcf2b-6f20-4585-b8b1-4a6f8a488820";
+    private static final String CONNECTION_STRING = "<Your Connection String>";
     private static final OpenTelemetry openTelemetry = initOpenTelemetry();
     private static final Meter meter = openTelemetry.getMeter("my meter");
 
