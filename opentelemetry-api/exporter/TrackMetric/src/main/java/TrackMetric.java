61c94620fdbb24b6f737f943022a1af9cea8ac18
Helen <56097766+heyams@users.noreply.github.com>
2024-09-30 10:32:15 -0700

    Repro (#534)

--- opentelemetry-api/exporter/TrackMetric/src/main/java/TrackMetric.java
+++ opentelemetry-api/exporter/TrackMetric/src/main/java/TrackMetric.java
@@ -1,9 +1,10 @@
 import com.azure.monitor.opentelemetry.exporter.AzureMonitorExporterBuilder;
 import com.azure.monitor.opentelemetry.exporter.implementation.SemanticAttributes;
-import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.metrics.DoubleGauge;
 import io.opentelemetry.api.metrics.DoubleHistogram;
 import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdkBuilder;
 
@@ -11,15 +12,25 @@ import java.util.UUID;
 
 public class TrackMetric {
     private static final String CONNECTION_STRING = "<Your Connection String>";
-    private static final OpenTelemetry openTelemetry = initOpenTelemetry();
+    private static final OpenTelemetrySdk openTelemetry = initOpenTelemetry();
     private static final Meter meter = openTelemetry.getMeter("my meter");
 
     public static void main(String[] args) throws InterruptedException {
-        trackDoubleHistogram();
+        trackDoubleHistogramWithCustomDimensions();
         Thread.sleep(5000); // wait at least 5 seconds to give batch span processor time to export
+
+        trackGauge();
+        Thread.sleep(5000); // wait at least 5 seconds to give batch span processor time to export
+
+        openTelemetry.shutdown();
     }
 
-    private static void trackDoubleHistogram() {
+    public static void trackGauge() throws InterruptedException {
+        DoubleGauge gauge = meter.gaugeBuilder("gauge" + UUID.randomUUID()).build();
+        gauge.set(111.0);
+    }
+
+    private static void trackDoubleHistogramWithCustomDimensions() {
         DoubleHistogram histogram =
            meter.histogramBuilder("histogram" + UUID.randomUUID())
                 .build();
@@ -32,7 +43,7 @@ public class TrackMetric {
         histogram.record(20.0, attributes);
     }
 
-    private static OpenTelemetry initOpenTelemetry() {
+    private static OpenTelemetrySdk initOpenTelemetry() {
         AutoConfiguredOpenTelemetrySdkBuilder sdkBuilder = AutoConfiguredOpenTelemetrySdk.builder();
         new AzureMonitorExporterBuilder()
             .connectionString(CONNECTION_STRING)
